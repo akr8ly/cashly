cd1187c2e8b48c8d78c6ac6b54c9b96d4fcb921a
akr8ly <avialok05@gmail.com>
2025-06-16 17:53:59 +0530

    Initial Django app setup

---
+++ cashly/cash_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CashAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cash_app'
--- cashly/cashly/asgi.py
+++ cashly/cashly/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cash project.
+ASGI config for cashly project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cash.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cashly.settings')
 
 application = get_asgi_application()
